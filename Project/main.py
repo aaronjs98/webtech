fe5619be5f55fbf0de72b81e18589980d9e2e033
aaronjs98 <aaronjs3998@gmail.com>
2023-01-09 14:32:49 +0000

    update project

--- Project/main.py
+++ Project/main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
-IMG_FOLDER = os.path.join('static', 'images')
-app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 @app.route('/')
 def index():
-    IMG_LIST = os.listdir('static/images')
-    IMG_LIST = ['images/'+ i for i in IMG_LIST]
-    return render_template('index.html', imagelist=IMG_LIST)
+    return render_template('index.html')
+
+@app.route('/ContactUs')
+def ContactUs():
+    return render_template('Contactus.html')
+
+@app.route('/Login')
+def Login():
+    return render_template('login.html')
+
+@app.route('/Register')
+def Register():
+    return render_template('Register.html')
 if __name__=='__main__':
     app.run(host='0.0.0.0', deabug=True)
